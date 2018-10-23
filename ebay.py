862c586e84b3b0347e92b6cef14aa424272fa3a1
Sam Rudd <samdrudd@gmail.com>
2018-10-22 21:17:57 -0700

    parses stuff, spits out csv file of SKUs and prices

--- ebay.py
+++ ebay.py
@@ -1,4 +1,5 @@
 import config
+import csv
 import requests
 
 baseurl = 'https://svcs.ebay.com/services/search/FindingService/v1'
@@ -20,5 +21,22 @@ if (r.status_code != 200):
 	print("EXITING")
 	exit()
 
+final_dict = {}
+	
 obj = r.json()
+
 pageInfo = obj['findItemsIneBayStoresResponse'][0]['paginationOutput'][0]
+totalPages = pageInfo['totalPages'][0]
+
+searchResults = obj['findItemsIneBayStoresResponse'][0]['searchResult'][0]
+
+for eachItem in searchResults['item']:
+	itemId = eachItem['itemId'][0]
+	final_dict[itemId] = {}
+	final_dict[itemId]['price']	= eachItem['sellingStatus'][0]['currentPrice'][0]['__value__']
+	
+with open('out.csv', 'w', newline='') as outfile:
+	writer = csv.writer(outfile)
+	writer.writerow(['SKU','PRICE'])
+	for eachItem in final_dict:
+		writer.writerow([eachItem, final_dict[eachItem]['price']])
